cd48f5d46764a8bbfc43648e62eab3eca60976ed
Endi S. Dewata <edewata@redhat.com>
2021-11-30 12:12:54 +0700

    Add JAVA_HOME for tools-test.sh

--- tests/bin/tools-test.sh
+++ tests/bin/tools-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+JAVA_HOME=/usr/lib/jvm/jre-11-openjdk
 CLASSPATH=/usr/share/java/ldapjdk.jar:/usr/share/java/slf4j/slf4j-api.jar:/usr/share/java/slf4j/slf4j-jdk14.jar
 
 echo "Checking Root DSE"
-java -cp $CLASSPATH \
+$JAVA_HOME/bin/java -cp $CLASSPATH \
     LDAPSearch -D "cn=Directory Manager" -w Secret.123 -b "" -s base "(objectClass=*)"
 
 if [ $? -ne 0 ]; then
@@ -12,7 +13,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Adding dc=test,dc=example,dc=com"
-java -cp $CLASSPATH \
+$JAVA_HOME/bin/java -cp $CLASSPATH \
     LDAPModify -D "cn=Directory Manager" -w Secret.123 -a << EOF
 dn: dc=test,dc=example,dc=com
 objectClass: dcObject
@@ -25,7 +26,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Verifying dc=test,dc=example,dc=com"
-java -cp $CLASSPATH \
+$JAVA_HOME/bin/java -cp $CLASSPATH \
     LDAPSearch -D "cn=Directory Manager" -w Secret.123 -b "dc=test,dc=example,dc=com" -s base "(objectClass=*)"
 
 if [ $? -ne 0 ]; then
@@ -34,7 +35,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Deleting dc=test,dc=example,dc=com"
-java -cp $CLASSPATH \
+$JAVA_HOME/bin/java -cp $CLASSPATH \
     LDAPDelete -D "cn=Directory Manager" -w Secret.123 "dc=test,dc=example,dc=com"
 
 if [ $? -ne 0 ]; then
@@ -43,7 +44,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Verifying dc=test,dc=example,dc=com"
-java -cp $CLASSPATH \
+$JAVA_HOME/bin/java -cp $CLASSPATH \
     LDAPSearch -D "cn=Directory Manager" -w Secret.123 -b "dc=test,dc=example,dc=com" -s base "(objectClass=*)"
 
 if [ $? -eq 0 ]; then
